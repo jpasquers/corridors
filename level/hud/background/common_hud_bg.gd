99f4066296eb8e5f4e355b9433f953cce09ba09e
Joseph Pasque <pasquej@wustl.edu>
2024-05-20 14:52:54 -0500

    fix: various textures

--- level/hud/background/common_hud_bg.gd
+++ level/hud/background/common_hud_bg.gd
@@ -3,11 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var orange_patches = $BG.material.get_shader_parameter("orange_patches");
+	var orange_patches = $Outer/BG.material.get_shader_parameter("orange_patches");
 	orange_patches.noise.seed = randi_range(0,10);
-	$BG.material.set_shader_parameter("orange_patches", orange_patches);
-	
-	$TopRope.material.set_shader_parameter("frequency", randi_range(1,8));
+	$Outer/BG.material.set_shader_parameter("orange_patches", orange_patches);
+	$Outer/TopRope.material.set_shader_parameter("frequency", randi_range(2,4));
 	pass # Replace with function body.
 
 
