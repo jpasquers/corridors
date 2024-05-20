82d9ed492144d1acaf41fee6f30b143bc3f445c5
Joseph Pasque <pasquej@wustl.edu>
2024-05-20 15:12:24 -0500

    fix: more textures

--- level/hud/background/common_hud_bg.gd
+++ level/hud/background/common_hud_bg.gd
@@ -6,7 +6,8 @@ func _ready():
 	var orange_patches = $Outer/BG.material.get_shader_parameter("orange_patches");
 	orange_patches.noise.seed = randi_range(0,10);
 	$Outer/BG.material.set_shader_parameter("orange_patches", orange_patches);
-	$Outer/TopRope.material.set_shader_parameter("frequency", randi_range(2,4));
+	$Outer/Rope1.material.set_shader_parameter("frequency", randi_range(3,4));
+	$Outer/Rope2.material.set_shader_parameter("frequency", randi_range(5,6));
 	pass # Replace with function body.
 
 
